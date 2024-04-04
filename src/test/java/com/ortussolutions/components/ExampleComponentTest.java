6a62d832e31b8334bf7b363f5cf324f677528871
Luis Majano <lmajano@gmail.com>
2024-04-04 13:33:10 +0200

    wip

--- src/test/java/com/ortussolutions/components/ExampleComponentTest.java
+++ src/test/java/com/ortussolutions/components/ExampleComponentTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import ortus.boxlang.parser.BoxScriptType;
+import ortus.boxlang.compiler.parser.BoxSourceType;
 import ortus.boxlang.runtime.BoxRuntime;
 import ortus.boxlang.runtime.context.IBoxContext;
 import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
@@ -17,30 +17,30 @@ import ortus.boxlang.runtime.scopes.VariablesScope;
 
 public class ExampleComponentTest {
 
-	static BoxRuntime	instance;
-	IBoxContext			context;
-	IScope				variables;
-	static Key			result	= new Key( "result" );
+    static BoxRuntime instance;
+    IBoxContext context;
+    IScope variables;
+    static Key result = new Key("result");
 
-	@BeforeAll
-	public static void setUp() {
-		instance = BoxRuntime.getInstance( true );
-	}
+    @BeforeAll
+    public static void setUp() {
+        instance = BoxRuntime.getInstance(true);
+    }
 
-	@BeforeEach
-	public void setupEach() {
-		context		= new ScriptingRequestBoxContext( instance.getRuntimeContext() );
-		variables	= context.getScopeNearby( VariablesScope.name );
-	}
+    @BeforeEach
+    public void setupEach() {
+        context = new ScriptingRequestBoxContext(instance.getRuntimeContext());
+        variables = context.getScopeNearby(VariablesScope.name);
+    }
 
-	@DisplayName( "It can test the ExampleComponent" )
-	@Test
-	public void testExampleComponent() {
-		instance.executeSource( """
-			<cfExampleComponent name="Ortus Solutions">
-			<cfset result = getBoxContext().getBuffer().toString()>
-		""", context, BoxScriptType.CFMARKUP );
-		assertTrue( variables.getAsString( result ).contains( "Hello, world - from Ortus Solutions." ) );
-	}
+    @DisplayName("It can test the ExampleComponent")
+    @Test
+    public void testExampleComponent() {
+        instance.executeSource("""
+                	<cfExampleComponent name="Ortus Solutions">
+                	<cfset result = getBoxContext().getBuffer().toString()>
+                """, context, BoxSourceType.CFTEMPLATE);
+        assertTrue(variables.getAsString(result).contains("Hello, world - from Ortus Solutions."));
+    }
 
 }
