deee5e483ae392aee136475fdc2438ddcbe72e73
Luis Majano <lmajano@gmail.com>
2024-10-29 17:33:31 +0100

    testing updates for all new modules

--- src/test/java/ortus/boxlang/moduleslug/IntegrationTest.java
+++ src/test/java/ortus/boxlang/moduleslug/IntegrationTest.java
@@ -1,65 +1,33 @@
+/**
+ * [BoxLang]
+ *
+ * Copyright [2023] [Ortus Solutions, Corp]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package ortus.boxlang.moduleslug;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import ortus.boxlang.runtime.BoxRuntime;
-import ortus.boxlang.runtime.context.IBoxContext;
-import ortus.boxlang.runtime.context.ScriptingRequestBoxContext;
-import ortus.boxlang.runtime.modules.ModuleRecord;
-import ortus.boxlang.runtime.scopes.IScope;
-import ortus.boxlang.runtime.scopes.Key;
-import ortus.boxlang.runtime.scopes.VariablesScope;
-import ortus.boxlang.runtime.services.DatasourceService;
-import ortus.boxlang.runtime.services.ModuleService;
 
 /**
  * This loads the module and runs an integration test on the module.
  */
-public class IntegrationTest {
-
-	static BoxRuntime		runtime;
-	static ModuleService	moduleService;
-	static Key				result	= new Key( "result" );
-	IBoxContext				context;
-	IScope					variables;
-
-	@BeforeAll
-	public static void setup() {
-		runtime			= BoxRuntime.getInstance( true, Path.of( "src/test/resources/boxlang.json" ).toString() );
-		moduleService	= runtime.getModuleService();
-	}
-
-	@BeforeEach
-	public void setupEach() {
-		context		= new ScriptingRequestBoxContext();
-		variables	= context.getScopeNearby( VariablesScope.name );
-	}
+public class IntegrationTest extends BaseIntegrationTest {
 
 	@DisplayName( "Test the module loads in BoxLang" )
 	@Test
 	public void testModuleLoads() {
 		// Given
-		Key					moduleName			= new Key( "@MODULE_SLUG@" );
-		String				physicalPath		= Paths.get( "./build/module" ).toAbsolutePath().toString();
-		ModuleRecord		moduleRecord		= new ModuleRecord( physicalPath );
-		IBoxContext			context				= new ScriptingRequestBoxContext();
-		DatasourceService	datasourceService	= runtime.getDataSourceService();
-
-		// When
-		moduleRecord
-		    .loadDescriptor( context )
-		    .register( context )
-		    .activate( context );
-
-		moduleService.getRegistry().put( moduleName, moduleRecord );
 
 		// Then
 		assertThat( moduleService.getRegistry().containsKey( moduleName ) ).isTrue();
