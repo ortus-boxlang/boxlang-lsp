59ac07c13babe78ccf9c4331c47299ca50bd9d5c
Jacob Beers <jbeers@ortussolutions.com>
2026-04-21 21:26:17 -0500

    Add support for user setting mappings

--- src/main/java/ortus/boxlang/lsp/UserSettings.java
+++ src/main/java/ortus/boxlang/lsp/UserSettings.java
@@ -5,6 +5,9 @@ import org.eclipse.lsp4j.MessageParams;
 import org.eclipse.lsp4j.MessageType;
 import org.eclipse.lsp4j.services.LanguageClient;
 
+import java.util.Map;
+
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import ortus.boxlang.lsp.config.annotation.ConfigGroup;
@@ -14,12 +17,14 @@ import ortus.boxlang.lsp.config.annotation.ConfigSetting;
 public class UserSettings {
 
 	@ConfigSetting( type = "boolean", description = "When true, triggers a workspace-wide parse and index of all BoxLang files on startup and when this setting changes. Improves symbol discovery at the cost of startup time.", defaultValue = "false", since = "1.0.0" )
-	private boolean			enableBackgroundParsing			= false;
+	private boolean				enableBackgroundParsing			= false;
 
 	@ConfigSetting( type = "boolean", description = "When true, lint diagnostics for open documents are calculated in parallel threads. Disable if you experience threading issues.", defaultValue = "true", since = "1.0.0" )
-	private boolean			processDiagnosticsInParallel	= true;
+	private boolean				processDiagnosticsInParallel	= true;
 
-	private LanguageClient	client							= null;
+	private LanguageClient		client							= null;
+
+	private Map<String, String>	mappings						= Map.of();
 
 	public void setLanguageClient( LanguageClient client ) {
 		this.client = client;
@@ -33,6 +38,10 @@ public class UserSettings {
 		return processDiagnosticsInParallel;
 	}
 
+	public Map<String, String> getMappings() {
+		return mappings;
+	}
+
 	public static UserSettings fromChangeConfigurationParams( LanguageClient client, DidChangeConfigurationParams params ) {
 		JsonObject		settings		= ( JsonObject ) params.getSettings();
 
@@ -40,6 +49,7 @@ public class UserSettings {
 		userSettings.client							= client;
 		userSettings.enableBackgroundParsing		= userSettings.checkBoolean( settings, "enableBackgroundParsing", false );
 		userSettings.processDiagnosticsInParallel	= userSettings.checkBoolean( settings, "processDiagnosticsInParallel", true );
+		userSettings.mappings						= userSettings.parseMappings( settings );
 
 		return userSettings;
 	}
@@ -62,5 +72,31 @@ public class UserSettings {
 			return defaultValue;
 		}
 
+	}
+
+	private Map<String, String> parseMappings( JsonObject settings ) {
+		try {
+			if ( !settings.has( "boxlang.mappings" ) ) {
+				return Map.of();
+			}
+
+			JsonElement mappingsElement = settings.get( "boxlang.mappings" );
+			if ( !mappingsElement.isJsonObject() ) {
+				this.client.logMessage( new MessageParams( MessageType.Error, "Unable to parse boxlang.mappings setting, defaulting to empty map" ) );
+				return Map.of();
+			}
+
+			JsonObject			mappingsObject	= mappingsElement.getAsJsonObject();
+			Map<String, String>	result			= new java.util.HashMap<>();
+			for ( java.util.Map.Entry<String, JsonElement> entry : mappingsObject.entrySet() ) {
+				if ( entry.getValue().isJsonPrimitive() && entry.getValue().getAsJsonPrimitive().isString() ) {
+					result.put( entry.getKey(), entry.getValue().getAsString() );
+				}
+			}
+			return Map.copyOf( result );
+		} catch ( Exception e ) {
+			this.client.logMessage( new MessageParams( MessageType.Error, "Unable to parse boxlang.mappings setting, defaulting to empty map" ) );
+			return Map.of();
+		}
 	}
 }
